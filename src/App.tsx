d04fa6b7dbbceabc86fb2dc6774e9dc197c25b7c
tiaan-jonker <tiaanj06@gmail.com>
2022-05-30 14:21:52 +1200

    unpkg plugin added to handle bundling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild-wasm'
 import React, { useEffect, useState, useRef } from 'react'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
@@ -27,12 +28,14 @@ const App = () => {
       return
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     })
 
-    setCode(result.code)
+    setCode(result.outputFiles[0].text)
   }
 
   return (
