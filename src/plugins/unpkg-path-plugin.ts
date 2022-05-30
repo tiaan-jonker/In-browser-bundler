0bdc7642411f500be2dd3a0eeab30d054e0c33f5
tiaan-jonker <tiaanj06@gmail.com>
2022-05-30 15:19:05 +1200

    naive case to bundle npm package for tests case

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,30 +1,44 @@
 import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // below hijacks edbuild's natural process of trying to find out
+      // where the file is stored
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args)
-        return { path: args.path, namespace: 'a' }
+        console.log('onResolve', args)
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' }
+        } 
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   }
+        // }
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args)
 
+        // if there's an attempt to load index.js then don't
         if (args.path === 'index.js') {
+          // rather return the below
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           }
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          }
+        }
+
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data,
         }
       })
     },
