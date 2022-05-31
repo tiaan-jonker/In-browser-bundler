e85b9b3421ee2ac2774a3565d17582c6730481d2
tiaan-jonker <tiaanj06@gmail.com>
2022-05-31 15:21:38 +1200

    changed process env setting and global

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,10 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     })
 
     setCode(result.outputFiles[0].text)
